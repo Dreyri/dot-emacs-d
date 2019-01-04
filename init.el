0882b23645092b2ef9bcc6bde801d79d2729622c
Frederik Engels <frederik.engels92@gmail.com>
2019-01-04 16:21:07 +0100

    ~ files begone!

--- init.el
+++ init.el
@@ -21,6 +21,15 @@
 ;; stop blinking
 (blink-cursor-mode -1)
 
+;; make specific directory for backups
+(setq
+ backup-by-copying t
+ backup-directory-alist '(("." . "~/.emacs.d/backup/"))
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)
+
 
 (setq-default truncate-lines nil)
 (setq-default word-wrap t)
