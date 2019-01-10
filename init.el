64c08246528639a4d31b8c718411b8c0a41e5172
Frederik Engels <frederik.engels92@gmail.com>
2019-01-10 06:11:07 +0100

    meson mode to edit meson build files

--- init.el
+++ init.el
@@ -572,6 +572,8 @@
   :mode (("CMakeLists.txt" . cmake-mode)
 	 ("\\.cmake\\'" . cmake-mode)))
 
+(use-package meson-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -581,5 +583,5 @@
  '(global-flycheck-mode t)
  '(package-selected-packages
    (quote
-    (org-journal ivy-youtube http evil-collection request-deferred deferred toml restclient json-rpc jsonrpc company-quickhelp-mode evil-smartparens ace-jump-buffer flycheck-popup-tip moe-theme ace-jump ace-jump-mode org-bullets golden-ratio projectile-ripgrep ripgrep lua-mode org-evil cargo magit-gh-pulls magit-gitflow git-timemachine evil-surround lispyville lispy evil-paredit evil-magit powerline evil-escape all-the-icons-ivy all-the-icons company-box geiser general cmake-mode gitconfig-mode nix-sandbox lsp-clangd ivy-xref paredit nix-buffer flycheck nix-mode nix-update yaml-mode yasnippet ccls company-lsp company projectile ace-window ivy-rich counsel ivy gitignore-mode magit clang-format org-plus-contrib hydra evil which-key delight use-package))))
+    (meson-mode org-journal ivy-youtube http evil-collection request-deferred deferred toml restclient json-rpc jsonrpc company-quickhelp-mode evil-smartparens ace-jump-buffer flycheck-popup-tip moe-theme ace-jump ace-jump-mode org-bullets golden-ratio projectile-ripgrep ripgrep lua-mode org-evil cargo magit-gh-pulls magit-gitflow git-timemachine evil-surround lispyville lispy evil-paredit evil-magit powerline evil-escape all-the-icons-ivy all-the-icons company-box geiser general cmake-mode gitconfig-mode nix-sandbox lsp-clangd ivy-xref paredit nix-buffer flycheck nix-mode nix-update yaml-mode yasnippet ccls company-lsp company projectile ace-window ivy-rich counsel ivy gitignore-mode magit clang-format org-plus-contrib hydra evil which-key delight use-package))))
 
