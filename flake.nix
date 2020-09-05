5e021c82b7ebc7a976766c3a37ae075fa3971bed
Frederik Engels <frederik.engels92@gmail.com>
2020-09-05 20:15:49 +0200

    ignore direnv and vscode directories

---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  description = "Dreyri's emacs configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+  };
+
+  outputs = inputs:
+    let
+      allSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      version = "${nixpkgs.lib.substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
+      emacsPackages = let epkgs = inputs.nixpkgs.legacyPackages.emacsPackagesFor 
+      extraPackages = epkgs: (with epkgs.elpaPackages; [
+        delight
+      ]) ++ (with epkgs.orgPackages; [
+        org-plus-contrib
+      ]) ++ (with epkgs.melpaPackages; [
+        format-all
+	build-farm
+	company-nixos-options
+	nix-mode
+	nix-update
+	nixos-options
+	nixpkgs-fmt
+	use-package
+	which-key
+	paredit
+	evil-paredit
+	lispy
+	evil
+	evil-collection
+	evil-escape
+	general
+	golden-ratio
+	org-journal
+	org-bullets
+	evil-org
+	modern-cpp-font-lock
+	magit
+	forge
+	magit-gh-pulls
+	magit-gitflow
+	evil-magit
+	git-commit
+	git-timemachine
+	gitignore-mode
+	ivy
+	ivy-xref
+	ivy-rich
+	swiper
+	avy
+	ace-window
+	ace-jump-mode
+	projectile
+	all-the-icons
+	all-the-icons-ivy
+	geiser
+	powerline
+	moe-theme
+	company
+	company-quickhelp
+	company-box
+	company-lsp
+	lsp-mode
+	lsp-ui
+	ccls
+	cquery
+	dap-mode
+	rust-mode
+	cargo
+	flycheck
+	flycheck-popup-tip
+	flycheck-rust
+	yasnippet
+        gitconfig-mode	
+	lua-mode
+	yaml-mode
+	cmake-mode
+	meson-mode
+      ]);
+
+      emacsPackages = emacs: final.emacsPackagesFor emacs;
+      emacsWithPackages = emacs: (emacsPackages emacs).emacsWithPackages;
+    in {
+      overlay = final: prev: {
+        emacs-dreyri = emacs: emacsWithPackages extraPackages;
+      };
+
+      defaultPackage = forAllSystems (system: (import nixpkgs {
+        inherit system;
+	overlays = [ self.overlay ];
+      }).emacs-dreyri);
+    };
+
+}
